a44c2d46bbea03deec3211552d22303a5c0c6afd
kokaneva <kokane.vina@gmail.com>
2023-05-08 11:22:29 +0530

    variables, loops, conditional

--- JavaScript/MyScripts/variables.js
+++ JavaScript/MyScripts/variables.js
@@ -15,11 +15,33 @@ console.log("datatype is:"+ typeof num2);
 //in same scope var type variables can be redeclared
 
 let i=1;
-let i=1;
+//let i=1;
 while(i<=5){
     console.log(i);
     i++;
 }
+for(let j=5;j>=1;j--){
+    console.log(j);
+}
 
+let myArray=[34,12,56,35,9,78,12,11];
 
+//for of loop
+    //1. forward, 0 to end of the array
+console.log("Normal for loop");
+for(let ai=0;ai<=myArray.length-1;ai++){
+ console.log(myArray[ai]);
+}
+console.log("For of loop");
 
+for(let ele of myArray){
+console.log(ele);
+}
+console.log("event elements");
+for(let ele of myArray){
+    if(ele%2==0)
+        console.log(ele);
+}
+
+const pie=3.142;
+//pie=3.14; can not change the value of const variables
